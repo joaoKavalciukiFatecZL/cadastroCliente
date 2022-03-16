7bcc73c8d43c7d750f6d836d32d754f8df4821ff
João <joao.sanches7@fatec.sp.gov.br>
2022-03-16 10:47:45 -0300

    Como enviar resposta dependendo da pagina que o usuário está

--- controller/routes/teste.js
+++ controller/routes/teste.js
@@ -1,5 +1,24 @@
-var http = require('http');
+const express = require('express');
 
-http.createServer().listen(8082)
+const app = express();
 
-console.log("O servidor está funcionando")
+app.get("/", function(req, resposta){
+    resposta.send("Home Page")
+})
+
+app.get("/contato", function(req, resposta){
+    resposta.send("Página de contato do app");
+});
+
+app.get("/produto", function(req, resposta){
+    resposta.send("Página de produtos")
+});
+
+app.get("/dados/:nome/:cargo", function(req, resposta){
+    resposta.send("<h1>Olá " + req.params.nome + "</h1> <h2>seu cargo é: " + req.params.cargo + "</h2>")
+})
+
+//ultima linha ao utilizar express, SEMPRE!
+app.listen(8082, function() {
+    console.log("O server está funcionando na url http://localhost:8082");
+});
